b88d864fd5fee32be73a61e64bb0d53ac7765b56
KingSit3 <i.ardiansyah93@gmail.com>
2021-02-16 04:09:54 +0700

    add sidebar list & icons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,7 @@
 
             {{-- SideBar List --}}
                 <div class="mt-3 mr-3 group">
-                    <a href="#">
+                    <a href="/">
                     <div class="w-full pl-1 py-2 group-hover:bg-indigo-800 rounded duration-150">
                         <div class="flex">
                             <svg class="transform translate- group-hover: group-hover:text-white duration-150 w-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -68,9 +68,9 @@
                     <div class="w-full pl-1 py-2 group-hover:bg-indigo-800 rounded duration-150">
                         <div class="flex">
                             <svg class="group-hover:text-white duration-150 w-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                             </svg>
-                            <span class="ml-3 group-hover:text-white flex items-center duration-150">Folder</span>
+                            <span class="ml-3 group-hover:text-white flex items-center duration-150">Perangkat</span>
                         </div>
                     </div>
                     </a>
@@ -80,9 +80,45 @@
                     <div class="w-full pl-1 py-2 group-hover:bg-indigo-800 rounded duration-150">
                         <div class="flex">
                             <svg class="group-hover:text-white duration-150 w-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                             </svg>
-                            <span class="ml-3 group-hover:text-white flex items-center duration-150">Report</span>
+                            <span class="ml-3 group-hover:text-white flex items-center duration-150">Tipe Perangkat</span>
+                        </div>
+                    </div>
+                    </a>
+                </div>
+                <div class="mt-1 mr-3 group">
+                    <a href="#">
+                    <div class="w-full pl-1 py-2 group-hover:bg-indigo-800 rounded duration-150">
+                        <div class="flex">
+                            <svg class="group-hover:text-white duration-150 w-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                            <span class="ml-3 group-hover:text-white flex items-center duration-150">Witel</span>
+                        </div>
+                    </div>
+                    </a>
+                </div>
+                <div class="mt-1 mr-3 group">
+                    <a href="#">
+                    <div class="w-full pl-1 py-2 group-hover:bg-indigo-800 rounded duration-150">
+                        <div class="flex">
+                            <svg class="group-hover:text-white duration-150 w-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
+                            </svg>
+                            <span class="ml-3 group-hover:text-white flex items-center duration-150">Image</span>
+                        </div>
+                    </div>
+                    </a>
+                </div>
+                <div class="mt-1 mr-3 group">
+                    <a href="#">
+                    <div class="w-full pl-1 py-2 group-hover:bg-indigo-800 rounded duration-150">
+                        <div class="flex">
+                            <svg class="group-hover:text-white duration-150 w-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+                            </svg>
+                            <span class="ml-3 group-hover:text-white flex items-center duration-150">Delivery Order</span>
                         </div>
                     </div>
                     </a>
