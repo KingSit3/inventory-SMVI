1bc12f4809cf8bdec8614783c9c20aaaeacded26
KingSit3 <i.ardiansyah93@gmail.com>
2021-02-15 19:13:20 +0700

    add softdelete to perangkat migration

--- database/migrations/2021_01_31_103239_create_perangkat_table.php
+++ database/migrations/2021_01_31_103239_create_perangkat_table.php
@@ -23,6 +23,7 @@ class CreatePerangkatTable extends Migration
             $table->bigInteger('id_image');
             $table->bigInteger('id_witel');
             $table->bigInteger('id_do');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
